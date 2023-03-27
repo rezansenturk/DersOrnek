8182481b56f1dfaf17f982604ef11141ba11008f
Rezan <116518302+rezansenturk@users.noreply.github.com>
2023-03-27 15:32:27 +0300

    AcunMedya Akademi Ders Projesi

--- DersÖrnek/Form1.Designer.cs
+++ DersÖrnek/Form1.Designer.cs
@@ -37,7 +37,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(266, 229);
+            this.button1.Location = new System.Drawing.Point(266, 207);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(124, 39);
             this.button1.TabIndex = 0;
