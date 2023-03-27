0bd88ab9c00ae4c02e7ef8254f61d1a785bd991e
Rezan <116518302+rezansenturk@users.noreply.github.com>
2023-03-27 15:23:33 +0300

    Add project files.

---
+++ DersÖrnek/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DersÖrnek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglan = new SqlConnection("Server=DESKTOP-R6345IK\\SQLEXPRESS;Database=MYUÖ;Trusted_Connection=True;Encrypt=False;");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglan.Open();
+            string query = "select * from Personeller where Email='" + textBox1.Text + "' and Sifre='" + textBox2.Text + "'";
+            SqlCommand cmd = new SqlCommand(query, baglan);
+            SqlDataReader reading = cmd.ExecuteReader();
+            
+
+            if (reading.Read())
+            {
+                this.Hide();
+                Form1 obj1 = new Form1();
+                obj1.Show();
+            }
+            else
+            {
+                MessageBox.Show("Hatalı giriş");
+                textBox1.Text = null;
+                textBox2.Text = null;
+            }
+            baglan.Close();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+    
+        }
+    }
+    
+}
